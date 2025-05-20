17c6740addb487861d06b7b3a3f37a0c2786e00f
Jonatan Churruarin <jonatan.churruarin@outlook.com>
2025-05-20 18:42:00 -0300

    cambios en ejericios de tp2

--- Ejercicios-de-Python/tp2-for.py
+++ Ejercicios-de-Python/tp2-for.py
@@ -1,1 +1,17 @@
 #trabajo practico numero 2
+suma=0
+contador=0
+nro=0
+contador2=0
+
+for i in range(1,6):
+    nro=int(input("Ingrese un numero: "))
+    print(f"Ingresaste el numero: {nro}")
+    if nro > 5:
+        contador=contador+1
+        print(f"El contador es: {contador}")
+    else:
+        contador2=contador2+1
+        print(f"El contador es: {contador2}")
+print(f"El total de numeros mayores a 5 es: {contador}")
+print(f"El total de numeros menores a 5 es: {contador2}")
