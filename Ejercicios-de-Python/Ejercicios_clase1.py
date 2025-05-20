bcf89f6944af1e0a250bd1ed72e71a3c1c0586e4
Jonatan Churruarin <jonatan.churruarin@outlook.com>
2025-05-20 17:32:27 -0300

    Mi primer subida en trabajos practicos

---
+++ Ejercicios-de-Python/Ejercicios_clase1.py
@@ -0,0 +1,44 @@
+"""
+Ejercicios Clase 1
+
+"""
+#Concatenacion Simple
+
+cadena1="Hola"
+cadena2=" mundo"
+
+cadena_concatenada=(cadena1+cadena2)
+
+print(cadena_concatenada)
+
+#output "Hola Mundo"
+
+#----------------------------------------------------------------------------------------------
+
+#Concatenacion con espacios.
+
+nombre="Juan"
+apellido="Perez"
+
+nombre_completo=(nombre+" "+apellido)
+
+print(nombre_completo)
+
+#Output "Juan Perez"
+
+#----------------------------------------------------------------------------------------------
+
+#Concatenacion de varias cadenas.
+
+frase1="El clima en"
+frase2="Vicente Lopez"
+frase3="es agradable"
+
+frase_completa=(frase1+" "+frase2+" "+frase3)
+
+print(frase_completa)
+
+#Output "El clima en Vicente Lopez es agradable"
+
+#----------------------------------------------------------------------------------------------
+
---
+++ Ejercicios-de-Python/Ejercicios_clase2.py
@@ -0,0 +1,112 @@
+"""
+Ejercicios Clase 2 
+
+"""
+#--------------------------------------------------------------------------------------------------
+#Empezamos en python
+#Funcion  Print con Strings
+print ("Hola mundo")
+print ("Buenas tardes")
+
+#--------------------------------------------------------------------------------------------------
+"""
+La funcion print se utiliza para mostrar en pantalla lo que debe ver el usuario.
+
+"""
+#Print con numeros enteros (Int)
+print (1+2)
+
+#Print con numeros con decimales (Float)
+print (1.2+2.5) 
+
+#--------------------------------------------------------------------------------------------------
+#Variables
+
+"""
+Las variables almacenan datos en memoria y los traen cuando las llamamos
+
+"""
+mi_apellido="Churruarin"
+
+print (mi_apellido)
+
+dia_de_hoy=20
+
+print (dia_de_hoy)
+
+print(dia_de_hoy+1)
+
+#---------------------------------------------------------------------------------------------------
+
+"""
+Ejercicios Practica de Clase.
+
+"""
+
+print("Hola")
+
+#Output "Hola"
+
+print("Clases Iniciadas")
+
+#Output "Clases Iniciadas"
+
+print(5+20)
+
+#Output "25"
+
+print(5*8)
+
+#Output "40"
+
+print ("5"+"20")
+
+#Output "520"
+
+nombre="Mario"
+
+print(nombre)
+
+#Output "Mario"
+
+print("Hola"+" "+nombre)
+
+#Output "Hola Mario"
+
+dia="25"
+
+print("Hoy es"+" "+dia+" "+"de"+" "+"Abril")
+
+#Output "Hoy es 25 de Abril"
+
+print("Viernes, fin de semana")
+
+#Output "Viernes, fin de semana"
+
+#--------------------------------------------------------------------------------------------
+
+"""
+Funcion Input
+
+"""
+#Es una Funcion que sirve para que el usuario ingrese datos por consola.
+
+nombre=input("Ingrese su nombre: ")
+
+print ("Hola, "+nombre)
+
+print(f"Hola, {nombre}")
+
+#---------------------------------------------------------------------------------------------
+
+diasrabajados=int(input("Dias que trabajo: "))
+pagodiario=int(input("Pago por día: "))
+pago_total=diasrabajados*pagodiario
+
+print(pago_total)
+
+print(f"El pago total es: {pago_total}")
+
+
+#----------------------------------------------------------------------------------------------
+
