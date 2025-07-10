0bfeb221208f976ab5e6ea17fdf57f2b4ccf6c37
jochurru <jonatan.churruarin@outlook.com>
2025-07-10 18:45:18 -0300

    commit de ejercicios python

---
+++ Trabajo practico n3/Fechas y horas/Ejercicio-2.py
@@ -0,0 +1,16 @@
+"""
+Ejemplo 2:
+Modificá el programa anterior para que muestre únicamente la fecha (sin la hora).
+
+"""
+import datetime
+# Obtener la fecha y hora actual
+fecha_hora_actual = datetime.datetime.now()
+# Mostrar la fecha actual
+print("Fecha actual:", fecha_hora_actual.date())
+# Mostrar la fecha actual en un formato más legible
+formato_legible = fecha_hora_actual.strftime("%Y-%m-%d")
+print("Fecha actual (formato legible):", formato_legible)
+# Mostrar la fecha actual en un formato personalizado
+formato_personalizado = fecha_hora_actual.strftime("%d/%m/%Y")
+print("Fecha actual (formato personalizado):", formato_personalizado)
